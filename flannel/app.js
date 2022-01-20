632ef463e3128199243e4933327de6aa1ee735e8
Henry Macarthur <henrymacarthur@Henrys-Macbook-Pro.local>
2022-01-19 17:58:51 -0800

    added mongodb connection

--- flannel/app.js
+++ flannel/app.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var path = require('path');
+let client = require('./db');
 
 const port = 3000;
+const url = 'mongodb+srv://<flannel>:<cs130sux>@cluster0.elmnm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 var app = express();
 
 var indexRouter = require('./routes/index');
@@ -12,4 +14,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 var server = app.listen(port, () => {
     console.log("server is running!");
+})
+
+client.connect('mongodb+srv://flannel:cs130sux@cluster0.elmnm.mongodb.net/flannel?retryWrites=true&w=majority', (err) => {
+  if(err)
+  {
+    console.log("error connecting to db");
+    process.exit(1);
+  }
+  else 
+  {
+    console.log("connected to database");
+  }
 })

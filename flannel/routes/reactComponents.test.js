47808f4bcbdca0aa41aea53a4c03ce9dcb6c265d
Brandon <chibrandon5@gmail.com>
2022-02-28 14:48:53 -0800

    add test for react

---
+++ flannel/routes/reactComponents.test.js
@@ -0,0 +1,7 @@
+const {react} = require('react');
+const {LogIn} = require('../src/pages/Login')
+describe('testing login and signup react functional components', () => {
+    it('loginUiTests', () => {
+        done();
+    })
+})

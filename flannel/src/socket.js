6a56e29bc2e3c2fcc5aa50869c6954f887d879ac
Collin Prince <collin@Collins-MBP-2.lan>
2022-03-02 11:59:33 -0800

    get rid of PROD config for socketio

--- flannel/src/socket.js
+++ flannel/src/socket.js
@@ -1,12 +1,6 @@
 import socketio from "socket.io-client"
 import React from 'react'
 
-// const port = process.env.PORT || 3000;
-const PROD = 1;
-let host = "http://localhost:3000/"
-if (PROD === 1) {
-  host = "https://flannel-ucla.herokuapp.com/";
-}
 
 export const socket = socketio({
     withCredentials: true,
